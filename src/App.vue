a30cb588cf4ef28c05ef517520be81ccfb87d24a
Tobias Weichart <tobias.weichart@gmail.com>
2020-07-11 17:34:58 +0100

    initial commit, change to vuetify via vue cli

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-app id="app">
+        <Banner/>
+    </v-app>
+</template>
+
+<script>
+import Banner from './components/Banner.vue'
+
+export default {
+    name: 'App',
+    components: {
+        Banner
+    }
+}
+</script>
+
+<style lang="scss">
+    #app{
+        height: 2000px;
+    }
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        name: 'Tobias Weichart',
+        jobtitle: 'Senior Software Engineer'
+    },
+    mutations: {},
+    actions: {},
+    modules: {}
+})
